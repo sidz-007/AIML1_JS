299b86029fa7f6a56b239bcc348f1ec056aa4a20
Mohd Sheeraz <sheerazmohd0803@gmail.com>
2026-02-20 10:31:08 +0530

    ScriptFile

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log("Hello, World!");
+// console.log("Hello, World!");
+// document.write("Welcome to AIML1_JS!");
+var num=10;
+console.log(num);
+console.log(typeof(num));
+
+num = false;
+console.log(num);
+console.log(typeof(num));
+
+num = "Mohd Sheeraz";
+console.log(num);
+console.log(typeof(num));
+
+num = null;
+console.log(num);
+console.log(typeof(num));
+
+num = undefined;
+console.log(num);
+console.log(typeof(num));
+
+const sym1 = Symbol(4);
+console.log(sym1);
+
+const sym2 = Symbol(4);
+console.log(sym2);
+
+if (sym1 === sym2) {
+    console.log("true");
+}
+else {    console.log("false");
+}
