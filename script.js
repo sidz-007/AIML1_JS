f60e01023c2a9edf87acc732cb2d5461f94bbec3
Mohd Sheeraz <sheerazmohd0803@gmail.com>
2026-02-20 10:31:56 +0530

    ScriptFile

--- script.js
+++ script.js
@@ -1,33 +1,37 @@
-// console.log("Hello, World!");
-// document.write("Welcome to AIML1_JS!");
-var num=10;
-console.log(num);
-console.log(typeof(num));
+// // console.log("Hello, World!");
+// // document.write("Welcome to AIML1_JS!");
+// var num=10;
+// console.log(num);
+// console.log(typeof(num));
 
-num = false;
-console.log(num);
-console.log(typeof(num));
+// num = false;
+// console.log(num);
+// console.log(typeof(num));
 
-num = "Mohd Sheeraz";
-console.log(num);
-console.log(typeof(num));
+// num = "Mohd Sheeraz";
+// console.log(num);
+// console.log(typeof(num));
 
-num = null;
-console.log(num);
-console.log(typeof(num));
+// num = null;
+// console.log(num);
+// console.log(typeof(num));
 
-num = undefined;
-console.log(num);
-console.log(typeof(num));
+// num = undefined;
+// console.log(num);
+// console.log(typeof(num));
 
-const sym1 = Symbol(4);
-console.log(sym1);
+// const sym1 = Symbol(4);
+// console.log(sym1);
 
-const sym2 = Symbol(4);
-console.log(sym2);
+// const sym2 = Symbol(4);
+// console.log(sym2);
 
-if (sym1 === sym2) {
-    console.log("true");
-}
-else {    console.log("false");
-}
+// if (sym1 === sym2) {
+//     console.log("true");
+// }
+// else {    console.log("false");
+// }
+var a = 100; var b = 13; var c = 10; var linebreak = "<br>";
+
+document.write("a+b+c = ");
+result = a + b + c;
